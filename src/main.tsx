ec28cc8e4e3f75383aec73c41783f830bcd9b894
Kate <Katezador-ipt27@lll.kpi.ua>
2025-07-24 01:55:53 +0300

    add edits

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import { Toaster } from 'react-hot-toast';
 // Нормалізація стилів
 import "modern-normalize";
 // Глобальні стилі (додатково)
@@ -11,7 +10,6 @@ import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Toaster position="top-right" />
     <App />
   </React.StrictMode>
 );
